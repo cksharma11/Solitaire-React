70209aa7ca68afc770bc860386817cfcc9682fe7
Chandan Kumar <22887527+cksharma11@users.noreply.github.com>
2019-04-03 11:54:56 +0530

    Extracted constant

--- src/game.js
+++ src/game.js
@@ -1,29 +1,37 @@
 import Deck from "./model/deck";
 import React from "react";
 
+const DECK_UNICODE = "\u{1F0A0}";
+
 class Game extends React.Component {
   constructor(props) {
     super(props);
     this.deck = new Deck();
     this.availaleCard = this.deck.drawACard();
-    this.state = { availaleCard: this.availaleCard}
+    this.state = { availaleCard: this.availaleCard };
   }
 
   render() {
-      return this.createDeckView();
+    return this.createDeckView();
   }
 
   createDeckView() {
-    return <div className="deck"> { 
-        <div className="pile">
-            <div onClick={this.drawACard.bind(this)} className="card" >{"\u{1F0A0}"}</div>
+    return (
+      <div className="deck">
+        {
+          <div className="pile">
+            <div onClick={this.drawACard.bind(this)} className="card">
+              {DECK_UNICODE}
+            </div>
             <div className="card">{this.state.availaleCard.unicode}</div>
-       </div>
-    } </div>;
+          </div>
+        }
+      </div>
+    );
   }
 
-  drawACard(){
-      return this.setState({ availaleCard: this.deck.drawACard()});
+  drawACard() {
+    return this.setState({ availaleCard: this.deck.drawACard() });
   }
 }
 
