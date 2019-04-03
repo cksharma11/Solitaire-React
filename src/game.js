64ec14e5b63b50546c7f5636d8120f4657a2589d
Chandan Kumar <22887527+cksharma11@users.noreply.github.com>
2019-04-03 12:20:01 +0530

    Added piles space

--- src/game.js
+++ src/game.js
@@ -18,14 +18,23 @@ class Game extends React.Component {
   createDeckView() {
     return (
       <div className="deck">
-        {
-          <div className="pile">
+        <div className="pile">
+          <div className="flex-wrap">
             <div onClick={this.drawACard.bind(this)} className="card">
               {DECK_UNICODE}
             </div>
-            <div className="card">{this.state.availaleCard.unicode}</div>
+            <div id="available-card" draggable className="card">
+              {this.state.availaleCard.unicode}
+            </div>
           </div>
-        }
+
+          <div className="flex-wrap">
+            <div id="pile1" className="card" />
+            <div id="pile2" className="card" />
+            <div id="pile3" className="card" />
+            <div id="pile4" className="card" />
+          </div>
+        </div>
       </div>
     );
   }
