13b8889d546e4bac439819cc05ddb5dcf74cbf95
Chandan Kumar <22887527+cksharma11@users.noreply.github.com>
2019-04-07 12:30:27 +0530

    Updates

---
+++ precache-manifest.157b7ce6f8b94e73b2e1a101544e792b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a794a68048fee6d8ec23",
+    "url": "/Solitaire-React/static/css/main.8da1281c.chunk.css"
+  },
+  {
+    "revision": "a794a68048fee6d8ec23",
+    "url": "/Solitaire-React/static/js/main.ce333def.chunk.js"
+  },
+  {
+    "revision": "adae973301a4a5ee4e18",
+    "url": "/Solitaire-React/static/js/runtime~main.d48e88d3.js"
+  },
+  {
+    "revision": "157f9282cb929b6ffec3",
+    "url": "/Solitaire-React/static/js/2.5b5d6d0c.chunk.js"
+  },
+  {
+    "revision": "71fb86c10574aa3436c57e4f2611a65c",
+    "url": "/Solitaire-React/index.html"
+  }
+];
