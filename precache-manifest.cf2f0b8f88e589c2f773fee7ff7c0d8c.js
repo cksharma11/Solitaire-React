85997325a77474ec8acf1d5555daa48cb3aad365
Chandan Kumar <22887527+cksharma11@users.noreply.github.com>
2019-04-07 18:57:30 +0530

    Updates

--- precache-manifest.cf2f0b8f88e589c2f773fee7ff7c0d8c.js
+++ precache-manifest.cf2f0b8f88e589c2f773fee7ff7c0d8c.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = [
   {
-    "revision": "a794a68048fee6d8ec23",
-    "url": "/Solitaire-React/static/css/main.8da1281c.chunk.css"
+    "revision": "bf62410f2cf16df2c9f1",
+    "url": "/Solitaire-React/static/css/main.45d9fe88.chunk.css"
   },
   {
-    "revision": "a794a68048fee6d8ec23",
-    "url": "/Solitaire-React/static/js/main.ce333def.chunk.js"
+    "revision": "bf62410f2cf16df2c9f1",
+    "url": "/Solitaire-React/static/js/main.daa2ba20.chunk.js"
   },
   {
     "revision": "adae973301a4a5ee4e18",
@@ -16,7 +16,7 @@ self.__precacheManifest = [
     "url": "/Solitaire-React/static/js/2.5b5d6d0c.chunk.js"
   },
   {
-    "revision": "71fb86c10574aa3436c57e4f2611a65c",
+    "revision": "39536a9948cabad81cfa46290d1e2837",
     "url": "/Solitaire-React/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/Solitaire-React/precache-manifest.157b7ce6f8b94e73b2e1a101544e792b.js"
+  "/Solitaire-React/precache-manifest.cf2f0b8f88e589c2f773fee7ff7c0d8c.js"
 );
 
 workbox.clientsClaim();
